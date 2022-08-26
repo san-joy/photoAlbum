398ee25b67d1a7ccee7906c69248e77080e07e4d
sanjoyak <san_joy007@yahoo.com>
2022-08-26 21:19:22 +0100

    Added end to end testing with cypress.
    - Two cypress e2e test added
    - Run "npx ng e2e photo-album-e2e --watch" to start the test

--- sanjoy-audhikari/apps/photo-album-e2e/src/e2e/app.cy.ts
+++ sanjoy-audhikari/apps/photo-album-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,13 @@
-import { getGreeting } from '../support/app.po';
+import { getListOfUsersTitle, getSubTitle } from '../support/app.po';
 
 describe('photo-album', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display title "List of users"', () => {
+    getListOfUsersTitle().contains("List of users");
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome photo-album');
+  it('should display subtitle "Click each row to check user\'s photo album"', () => {
+    getSubTitle().contains("Click each row to check user's photo album");
   });
 });
